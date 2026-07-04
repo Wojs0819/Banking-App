3fdcf477427da55f7ad3ab3f6fd15bbfcb55fbaa
Wojs0819 <jswong0819@gmail.com>
2026-07-04 17:43:02 +0800

    set up backend spring boot environment

---
+++ backend/src/test/java/com/bankApp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankApp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
